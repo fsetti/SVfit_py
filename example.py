dc2a3cdd7d2d55c016914fc9ec1cd3e8480d2c11
Franny Setti fsetti@ucsb.edu 9/4/2018 <fsetti@uaf-10.t2.ucsd.edu>
2020-12-01 19:21:42 -0800

    first commit

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 import ROOT
 from ROOT import gROOT
 
-gROOT.ProcessLine(".L /home/users/fsetti/CMSSW_10_2_9/lib/slc6_amd64_gcc700/libTauAnalysisClassicSVfit.so")
-gROOT.ProcessLine(".L /home/users/fsetti/CMSSW_10_2_9/lib/slc6_amd64_gcc700/libTauAnalysisSVfitTF.so")
+gROOT.ProcessLine(".L /home/users/fsetti/CMSSW_9_4_4/lib/slc6_amd64_gcc700/libTauAnalysisClassicSVfit.so")
+gROOT.ProcessLine(".L /home/users/fsetti/CMSSW_9_4_4/lib/slc6_amd64_gcc700/libTauAnalysisSVfitTF.so")
 gROOT.ProcessLine(".L SVfit_py.cc+")
 
 ROOT.SVfit_mass( parameters )
