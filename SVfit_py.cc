6203c167d456eed976da51f50adece9233e61336
Franny Setti fsetti@ucsb.edu 9/4/2018 <fsetti@uaf-10.t2.ucsd.edu>
2021-02-09 15:36:55 -0800

    include individual tau p4

--- SVfit_py.cc
+++ SVfit_py.cc
@@ -39,3 +39,96 @@ double SVfit_mass(float measuredMETx, float measuredMETy, float covMET_XX, float
 
 	return diTauSVfit_mass;
 }
+
+
+vector<double>  SVfit_ditau_p4(float measuredMETx, float measuredMETy, float covMET_XX, float covMET_XY, float covMET_YY,int tauDecay_mode1, int tauDecay_mode2, int category1, int category2, float tau1_pt,float  tau1_eta, float tau1_phi, float tau1_mass, float tau2_pt, float tau2_eta, float tau2_phi, float tau2_mass)
+{
+	double diTauSVfit_mass;
+
+	TMatrixD covMET(2, 2);
+	covMET[0][0] = covMET_XX;
+	covMET[0][1] = covMET_XY;
+	covMET[1][0] = covMET_XY;
+	covMET[1][1] = covMET_YY;
+
+	// define lepton four vectors
+	std::vector<MeasuredTauLepton> measuredTauLeptons;
+	if ( category1 == 1 ){
+		measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToMuDecay, tau1_pt, tau1_eta, tau1_phi, 0.10566));
+	}
+	else if ( category1 == 2 ){
+		measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToElecDecay, tau1_pt, tau1_eta, tau1_phi, 0.51100e-3));
+	}
+	else {
+		measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToHadDecay, tau1_pt, tau1_eta, tau1_phi, tau1_mass, tauDecay_mode1));
+	}
+
+	measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToHadDecay,  tau2_pt, tau2_eta, tau2_phi,  tau2_mass, tauDecay_mode2));
+
+	//Run FastMTT
+	FastMTT aFastMTTAlgo;
+	aFastMTTAlgo.run(measuredTauLeptons, measuredMETx, measuredMETy, covMET);
+	LorentzVector ttP4 = aFastMTTAlgo.getBestP4();
+	//diTauSVfit_mass = ttP4.M();
+	
+	vector<double> tt_p4(4,0.);
+	tt_p4[0]	= ttP4.Px();
+	tt_p4[1]	= ttP4.Py();
+	tt_p4[2]	= ttP4.Pz();
+	tt_p4[3]	= ttP4.E();
+	return tt_p4;
+}
+
+
+vector<vector<double>> SVfit_all_p4(float measuredMETx, float measuredMETy, float covMET_XX, float covMET_XY, float covMET_YY,int tauDecay_mode1, int tauDecay_mode2, int category1, int category2, float tau1_pt,float  tau1_eta, float tau1_phi, float tau1_mass, float tau2_pt, float tau2_eta, float tau2_phi, float tau2_mass)
+{
+	double diTauSVfit_mass;
+
+	TMatrixD covMET(2, 2);
+	covMET[0][0] = covMET_XX;
+	covMET[0][1] = covMET_XY;
+	covMET[1][0] = covMET_XY;
+	covMET[1][1] = covMET_YY;
+
+	// define lepton four vectors
+	std::vector<MeasuredTauLepton> measuredTauLeptons;
+	if ( category1 == 1 ){
+		measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToMuDecay, tau1_pt, tau1_eta, tau1_phi, 0.10566));
+	}
+	else if ( category1 == 2 ){
+		measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToElecDecay, tau1_pt, tau1_eta, tau1_phi, 0.51100e-3));
+	}
+	else {
+		measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToHadDecay, tau1_pt, tau1_eta, tau1_phi, tau1_mass, tauDecay_mode1));
+	}
+
+	measuredTauLeptons.push_back(MeasuredTauLepton(MeasuredTauLepton::kTauToHadDecay,  tau2_pt, tau2_eta, tau2_phi,  tau2_mass, tauDecay_mode2));
+
+	//Run FastMTT
+	FastMTT aFastMTTAlgo;
+	aFastMTTAlgo.run(measuredTauLeptons, measuredMETx, measuredMETy, covMET);
+	LorentzVector ttP4 = aFastMTTAlgo.getBestP4();
+	//diTauSVfit_mass = ttP4.M();
+	
+	vector<double> tt_p4(4,0.);
+	tt_p4[0]	= ttP4.Px();
+	tt_p4[1]	= ttP4.Py();
+	tt_p4[2]	= ttP4.Pz();
+	tt_p4[3]	= ttP4.E();
+
+	LorentzVector tau1P4 = aFastMTTAlgo.getTau1P4();	
+	vector<double> tau1_p4(4,0.);
+	tau1_p4[0]	= tau1P4.Px();
+	tau1_p4[1]	= tau1P4.Py();
+	tau1_p4[2]	= tau1P4.Pz();
+	tau1_p4[3]	= tau1P4.E();
+
+	LorentzVector tau2P4 = aFastMTTAlgo.getTau2P4();	
+	vector<double> tau2_p4(4,0.);
+	tau2_p4[0]	= tau2P4.Px();
+	tau2_p4[1]	= tau2P4.Py();
+	tau2_p4[2]	= tau2P4.Pz();
+	tau2_p4[3]	= tau2P4.E();
+	
+	return  { tt_p4 , tau1_p4 , tau2_p4 } ;
+}
--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import ROOT
+import numpy as np
 from ROOT import gROOT
 
 gROOT.ProcessLine(".L libTauAnalysisClassicSVfit.so")
@@ -10,4 +11,18 @@ gROOT.ProcessLine(".L libTauAnalysisSVfitTF.so")
 
 gROOT.ProcessLine(".L SVfit_py.cc+")
 
-diTau_mass = ROOT.SVfit_mass( parameters )
+diTau_mass = ROOT.SVfit_mass( 11.7491,-51.9172, 787.352,-178.63,179.545, -1,0, 2,3, 33.7393,0.9409,-0.541458,0.51100e-3, 25.7322,0.618228,2.79362,0.13957 )
+print ( ' di-tau mass: ' , diTau_mass )
+
+
+diTau_p4 = ROOT.SVfit_ditau_p4( 11.7491,-51.9172, 787.352,-178.63,179.545, -1,0, 2,3, 33.7393,0.9409,-0.541458,0.51100e-3, 25.7322,0.618228,2.79362,0.13957 )
+print ( ' diTau_p4 ' ,np.sqrt( diTau_p4[3] * diTau_p4[3] - diTau_p4[0] * diTau_p4[0] - diTau_p4[1] * diTau_p4[1] - diTau_p4[2] * diTau_p4[2] ) )
+
+
+parts_p4 = ROOT.SVfit_all_p4( 11.7491,-51.9172, 787.352,-178.63,179.545, -1,0, 2,3, 33.7393,0.9409,-0.541458,0.51100e-3, 25.7322,0.618228,2.79362,0.13957 )
+tt_p4 	= parts_p4[0]
+tau1_p4 = np.array(parts_p4[1])
+tau2_p4 = np.array(parts_p4[2])
+tautau_p4 = tau1_p4 + tau2_p4
+print ( ' tt_p4 ' , np.sqrt( tt_p4[3] * tt_p4[3] - tt_p4[0] * tt_p4[0] - tt_p4[1] * tt_p4[1] - tt_p4[2] * tt_p4[2] ) )
+print ( ' tau1_p4 + tau2_p4 ' , np.sqrt( tautau_p4[3] * tautau_p4[3] - tautau_p4[0] * tautau_p4[0] - tautau_p4[1] * tautau_p4[1] - tautau_p4[2] * tautau_p4[2] ) )
